61c510231cfe368f48b4545483f1b4ed1e59e68f
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-04-19 14:57:37 +0200

    Update RunAssignmentInsurance.m

--- RunAssignmentInsurance.m
+++ RunAssignmentInsurance.m
@@ -36,18 +36,26 @@ filename = 'EIOPA_RFR_20240331_Term_Structures.xlsx';
 
 Sheet = 'RFR_spot_no_VA';
 
+rates_UP = xlsread(filename,5,'S11:S160');
+rates_DOWN = xlsread(filename,6,'S11:S160');
+
 rates = xlsread(filename,1,'S11:S160');
 
 disp(rates)
 
 % plot yield rate curve
 figure
+hold on 
 years = 1:150;
 plot(years,rates)
+plot(years,rates_UP)
+plot(years,rates_DOWN)
+rate_50 = interp1(years,rates,50);
+disp(rate_50)
+plot(50,rate_50,'ro')
+legend( 'rates','rates_{DOWN}','rates_{UP}')
 
 % plot mortality table
 figure
 plot([1:24],Life_exp)
-
-%tolo gei
 
