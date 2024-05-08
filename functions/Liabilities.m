fb131626fc226ec6606389703d4d7492d0a0bfe4
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-05-08 09:51:49 +0200

    update on excel and folders

---
+++ functions/Liabilities.m
@@ -0,0 +1,73 @@
+function [liabilities, Lapse_BEL, Death_BEL, Expenses_BEL,Commissions_BEL] = Liabilities(F0, P_death, lt, regular_deduction, COMM, discounts, expenses,dt,F, benefit_commission,T)
+    % Computes the liabilities and split the BEL value into its main PV components: 
+    % death benefits, lapse benefits, expenses and commissions
+    %       
+    % INPUTS
+    % F0: initial fund
+    % P_death: probability of death
+    % lt: probability of lapse
+    % RD: regular deduction
+    % COMM: commission
+    % discounts: discounts
+    % expenses: expenses per year
+    % dt: time steps
+    % F: fund value
+    % benefit_commission: benefit commission
+    % T: time to maturity
+    %
+    % OUTPUTS
+    % liabilities: liabilities of the insurance company
+    % main PV components of the BEL value:
+    % Lapse_BEL 
+    % Death_BEL
+    % Expenses_BEL
+    % Commissions_BEL
+
+    % Initialize variables
+    Expenses = zeros(size(dt));
+    Contract_prob = cumprod([1; (1-P_death(1:end-1)).*(1-lt(1:end-1))]);
+    
+    % Initialize waitbar
+    h = waitbar(0, 'Calculating...');
+
+    for i = 1:length(dt)
+        % cash flows at each year
+        death_cf = (max(F0, F(:,i+1))) * P_death(i) * (1-lt(i));     % TODO se togli 1-lt viene deltaBOFCAT positivo
+        
+        % at the end of the 50 years all the people leave the contract with a massive surrender
+        if i == T   % condition because in the last year it's mandatory to lapse
+            lapse_cf = (F(:,i+1) - benefit_commission) * (1 - P_death(i));
+        else
+            lapse_cf = (F(:,i+1) - benefit_commission) * lt(i) * (1 - P_death(i));
+        end
+
+        Expenses(i) = Contract_prob(i) * expenses(i);
+        Lapse_benefits(:,i) = Contract_prob(i) * lapse_cf;
+        Death_benefits(:,i) = Contract_prob(i) * death_cf;
+        Commissions(:,i) = Contract_prob(i) * F(:,i) / (1 - regular_deduction) * COMM;
+
+        Val(:,i) = Lapse_benefits(:,i) +  Death_benefits(:,i) + Expenses(i) + Commissions(:,i);
+        
+        % Update waitbar
+        waitbar(i / length(dt), h, sprintf('Calculating... %d%%', round(i / length(dt) * 100)));
+    end
+
+    % Close waitbar after loop completes
+    close(h);
+
+    % computation of the death_BEL 
+    Death_BEL=(mean(Death_benefits)*discounts);
+     
+    % computation of the lapse_BEL
+    Lapse_BEL=(mean(Lapse_benefits)*discounts);
+
+    % computation of expenses_BEL
+    Expenses_BEL=(Expenses'*discounts);
+
+    % computation of commissions_BEL
+    Commissions_BEL=(mean(Commissions)*discounts);
+    
+    % computation of the liabilities
+    liabilities = sum(mean(Val)* discounts);
+
+    end % function Liabilities
